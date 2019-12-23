4dc13703e52006f520604d0ce85c005997398f09
sammychinedu2ky <sammychinedu2ky@gmail.com>
2019-12-23 18:27:36 +0100

    hello

--- sw.js
+++ sw.js
@@ -69,3 +69,9 @@ self.addEventListener('fetch', function(e) {
   );
 });
 
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can add to home screen
+  showInstallPromotion();
+});
